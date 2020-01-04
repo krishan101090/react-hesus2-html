ca4677f6da2e84ef6c4c57790d5c061fad66bc98
krishan101090 <krishan101090@gmail.com>
2020-01-04 14:10:42 +0530

    3 files modified

--- index.js
+++ index.js
@@ -16,9 +16,70 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <header className="header">
-        <img src={imageLogo}/>
+        <header>
+          <div className="image-wrap">
+            <img src={imageLogo}/>
+          </div>
+          <div className="search-wrap">
+            <input placeholder="Search..." />
+          </div>
         </header>
+        <main>
+          <section className="main-dec">
+          <section className="news-feeds">
+            <h1><strong>News Feed</strong></h1>
+            <article className="new-card-wrap">
+              <div className="color-strip"></div>
+              <div className="news-box">
+                <div className="rect-box"></div>
+                <div className="news-info">
+                <div className="title">CENTRALISE</div>
+                <hr/>
+                <div className="bottom-info"><div className="date-style">Oct 27, 2019</div><div className="name-style">Jack</div></div>
+                </div>
+              </div>
+            </article>
+            <article className="new-card-wrap">
+              <div className="color-strip orange-strip"></div>
+              <div className="news-box">
+                <div className="rect-box"></div>
+                <div className="news-info">
+                <div className="title">CENTRALISE</div>
+                <hr/>
+                <div className="bottom-info"><div className="date-style">Oct 27, 2019</div><div className="name-style">Jack</div></div>
+                </div>
+              </div>
+            </article>
+            <article className="new-card-wrap">
+              <div className="color-strip light-blue"></div>
+              <div className="news-box">
+                <div className="rect-box"></div>
+                <div className="news-info">
+                <div className="title">CENTRALISE</div>
+                <hr/>
+                <div className="bottom-info"><div className="date-style">Oct 27, 2019</div><div className="name-style">Jack</div></div>
+                </div>
+              </div>
+            </article>
+          </section>
+            <article className="welcome-dec">
+              <h1>Welcome <strong>Jack</strong></h1>
+              <p>We are always looking forward to meet new talants! You are willing to become part of the  advanture and support us in the development of our activity? Dont hesitate anyomore and join us!</p>
+            </article>
+
+            <article className="about-us">
+            <div className="color-strip"></div>
+              <h1><strong>About Us</strong></h1>
+              <p>Safe garding the environment has become a core concern of the developemnt of the large cities. <a><strong>View More</strong></a></p>
+            </article>
+          </section>
+          
+          <section className="registration-wrap">
+          <h1>Registration</h1>
+          <form>
+          </form>
+          </section>
+        </main>
       </div>
     );
   }
