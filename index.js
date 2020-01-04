cccb7853bb4c2d6cad8ffeeb3a162184444615d4
krishan101090 <krishan101090@gmail.com>
2020-01-04 15:08:25 +0530

    2 files modified

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ import { render } from 'react-dom';
 import Hello from './Hello';
 import imageLogo from './hesus.js';
 import './reset.scss'
-import './breakpoint.scss';
-import './grid.scss';
 import './index.scss';
 
 class App extends Component {
@@ -19,15 +17,17 @@ class App extends Component {
     return (
       <div className="app">
         <header>
+        <div className="header-content-wrap">
           <div className="image-wrap">
             <img src={imageLogo}/>
           </div>
           <div className="search-wrap">
             <input placeholder="Search..." />
           </div>
+          </div>
         </header>
-        <main>
-          <section className="main-dec">
+        <main className="col-12">
+          <section className="main-dec col-4">
             <article className="welcome-dec">
               <h1>Welcome <strong>Jack</strong></h1>
               <p>We are always looking forward to meet new talants! You are willing to become part of the  advanture and support us in the development of our activity? Dont hesitate anyomore and join us!</p>
@@ -39,7 +39,8 @@ class App extends Component {
               <p>Safe garding the environment has become a core concern of the developemnt of the large cities. <a><strong>View More</strong></a></p>
             </article>
           </section>
-          <section className="news-feeds">
+          <hr className="divider" />
+          <section className="news-feeds col-4">
             <h1><strong>News Feed</strong></h1>
             <article className="new-card-wrap">
               <div className="color-strip"></div>
@@ -75,9 +76,26 @@ class App extends Component {
               </div>
             </article>
           </section>
-          <section className="registration-wrap">
-          <h1>Registration</h1>
+          <hr className="divider" />
+          <section className="registration-wrap col-3">
+          <h1><strong>Registration</strong></h1>
           <form>
+            <div className="input-wrap">
+            <label>First Name</label>
+            <input />
+            </div>
+            <div className="input-wrap">
+            <label>Email</label>
+            <input />
+            </div>
+            <div className="input-wrap">
+            <label>Password</label>
+            <input />
+            </div>
+            <div className="button-container">
+              <button className="cancel-btn">Cancel</button>
+              <button className="submit-btn">Submit</button>
+            </div>
           </form>
           </section>
         </main>
